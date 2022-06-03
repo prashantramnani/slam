9520409373dc542238fd6f37ebaa8b6ab8df1d3d
Prashant Ramnani <ramnani.prashant@gmail.com>
2022-06-03 13:18:33 +0530

    Added primitive ORB detection

--- slam.py
+++ slam.py
@@ -21,20 +21,26 @@ surface = screen.set_mode([W, H])
 
 orb = cv2.ORB_create()
 
-def process_frame(frame):
-    frame = cv2.resize(frame, (W, H))
-    #frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    kp = orb.detect(frame,None)
-    kp, des = orb.compute(frame, kp)
-    img2 = cv2.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
-    plt.imshow(img2), plt.show()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+def paint(frame):
     surf = pygame.surfarray.make_surface(frame)
     surface.blit(surf, (0,0))
     screen.update()
+
+def process_frame(frame):
+    frame = cv2.resize(frame, (W, H))
+    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    kp, des = orb.detectAndCompute(frame,None)
+    #img2 = cv2.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
+    #plt.imshow(img2), plt.show()
+    for p in kp:
+        u, v = map(lambda x : int(round(x)), p.pt)
+        cv2.circle(frame, (u, v), color=(0,255,0), radius=3)
+    #print(kp[0].pt)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    paint(frame)
+
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(video_path)
