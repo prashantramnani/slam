c68be35e6c76e3461a20f179ec1af2d2144680f4
Prashant Ramnani <ramnani.prashant@gmail.com>
2022-06-07 02:52:19 +0530

    little bit of refactoring

--- slam.py
+++ slam.py
@@ -1,8 +1,11 @@
 #!/usr/local/bin/python3
+
 import cv2
 import pygame
 from matplotlib import pyplot as plt
 import numpy as np
+from surface import Surface
+from extractor import FeatureExtractor
 #import sdl2.ext
 
 video_path = "/Users/prashantramnani/Projects/slam/test.mp4"
@@ -15,20 +18,14 @@ divide_h = 5
 divide_w = 5 
 
 pygame.init()
+surface = Surface(W, H)
+fe = FeatureExtractor()
 
-#sdl2.ext.init()
-#window = sdl2.ext.Window("Hello World!", size=(640, 480))
-#window.show()
-
-screen = pygame.display
-surface = screen.set_mode([W, H])
-
-orb = cv2.ORB_create(100)
-
-def paint(frame):
-    surf = pygame.surfarray.make_surface(frame)
-    surface.blit(surf, (0,0))
-    screen.update()
+'''
+sdl2.ext.init()
+window = sdl2.ext.Window("Hello World!", size=(640, 480))
+window.show()
+'''
 
 def process_frame(frame):
     frame = cv2.resize(frame, (W, H))
@@ -45,18 +42,19 @@ def process_frame(frame):
                 kp_frag.append((p.pt[0] + w, p.pt[1] + h))    
     '''
 
-    #kp = cv2.goodFeaturesToTrack(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 5000, qualityLevel=0.01, minDistance=3)  
-    kp = cv2.goodFeaturesToTrack(np.mean(frame, axis=2).astype(np.uint8), 5000, qualityLevel=0.05, minDistance=2)
-    corners = np.int0(kp)
+    kp = fe.goodFeaturesExtract(frame)
     for p in kp:
         #u, v = map(lambda x : int(round(x)), p)
-        x, y = p.ravel()
-        cv2.circle(frame, (x, y), color=(0,255,0), radius=2)
+        u, v = p.ravel()
+        print("u {}, v {}".format(u, v))
+        cv2.circle(frame, (u, v), color=(0,255,0), radius=2)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
     frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    paint(frame)
+    surface.paint(frame)
 
 
 if __name__ == "__main__":
